08144845f43c575227ea5d115667cbf9aa43feb7
awwar <you@example.com>
2023-10-30 00:28:10 +0300

    feat: refactor flow creation

--- lib/masterpiece/parsers/scope_parser.ex
+++ lib/masterpiece/parsers/scope_parser.ex
@@ -2,7 +2,7 @@ defmodule ScopeParser do
   def parse(id, scope, options) do
     scope_pattern = ScopePatternFactory.create(scope)
 
-    %Types.ScopeSocket {
+    %Types.ScopeSocket{
       id: SocketReferenceParser.parse(id),
       scope: scope_pattern,
       options: scope_pattern.parse_options(options)
